6635267f83d90681fb0c5c0fbd44118e62485d3e
alexjolong <alexjolong@gmail.com>
2020-04-06 20:39:14 -0500

    Updated project file structure

--- bash/deploy-proj2.sh
+++ bash/deploy-proj2.sh
@@ -1,13 +1,66 @@
-#!/bin/sh
-echo "Making Hadoop Folders for Impala Load..."
-sudo -u hdfs hdfs dfs -mkdir -p '/user/hive/warehouse/raw_data/Customers/'
-sudo -u hdfs hdfs dfs -mkdir -p '/user/hive/warehouse/raw_data/Employees/'
-sudo -u hdfs hdfs dfs -mkdir -p '/user/hive/warehouse/raw_data/Products/'
-sudo -u hdfs hdfs dfs -mkdir -p '/user/hive/warehouse/raw_data/Sales/'
-echo "Putting data into Hadoop Distributed File System..."
-sudo -u hdfs hdfs dfs -put ./share/data/salesdb/Customers2.csv '/user/hive/warehouse/raw_data/Customers/Customers.csv'
-sudo -u hdfs hdfs dfs -put ./share/data/salesdb/Employees2.csv '/user/hive/warehouse/raw_data/Employees/Employees.csv'
-sudo -u hdfs hdfs dfs -put ./share/data/salesdb/Products.csv '/user/hive/warehouse/raw_data/Products/Products.csv'
-sudo -u hdfs hdfs dfs -put ./share/data/salesdb/Sales2.csv '/user/hive/warehouse/raw_data/Sales/Sales.csv'
-echo "Make Impala/Hive the owner of our raw data in HDFS"
-sudo -u hdfs hdfs dfs -chown -R hive:hive "/user/hive/warehouse/raw_data"
+#!/usr/bin/env
+
+# Global variables
+ext4_data_directory=~/data
+hdfs_data_directory=/data
+option_count=0
+option=$1  # first argument passed, if any
+
+# TODO: If file already exists, produce warning at this step
+download_data() {
+    echo "Getting sales data from AWS..."
+    wget https://csci5751-2020sp.s3-us-west-2.amazonaws.com/sales-data/salesdata.tar.gz
+    echo "Sales data retrieved, unzipping..."
+    tar -xvzf salesdata.tar.gz
+    mv salesdb $ext4_data_directory
+    echo "Done unzipping, cleaning up..."
+    rm salesdata.tar.gz
+}
+
+load_data() {
+    echo "Making Hadoop Folders for Impala Load..."
+    sudo -u hdfs hdfs dfs -mkdir -p $hdfs_data_directory/Customers/
+    sudo -u hdfs hdfs dfs -mkdir -p $hdfs_data_directory/Employees/
+    sudo -u hdfs hdfs dfs -mkdir -p $hdfs_data_directory/Products/
+    sudo -u hdfs hdfs dfs -mkdir -p $hdfs_data_directory/Sales/
+
+    echo "Putting data into Hadoop Distributed File System..."
+    sudo -u hdfs hdfs dfs -put $ext4_data_directory/Customers2.csv $hdfs_data_directory/Customers/
+    sudo -u hdfs hdfs dfs -put $ext4_data_directory/Employees2.csv $hdfs_data_directory/Employees/
+    sudo -u hdfs hdfs dfs -put $ext4_data_directory/Products.csv $hdfs_data_directory/Products/
+    sudo -u hdfs hdfs dfs -put $ext4_data_directory/Sales2.csv $hdfs_data_directory/Sales/
+
+    echo "Make Impala/Hive the owner of our raw data in HDFS"
+    sudo -u hdfs hdfs dfs -chown -R hive:hive $hdfs_data_directory
+}
+
+
+# Parse input arguments and execute
+while [ $option_count -eq 0 ]; do
+    option_count=$(( option_count + 1 ))
+
+    case $option in
+        -d | --download_data)
+            download_data
+            ;;
+        
+        -l | --load_data)
+            load_data
+            ;;
+
+        --) # User indicating no more options
+            shift # shift this argument off the array
+            break # get out of the case statement
+            ;;
+        
+        -*) # Any argument we don't expect
+            echo "Unknown option provided: $1" >&2 # pipe to stderr (&2 file descriptor)
+            exit 1
+            ;;
+
+        *) # Nothing
+            break
+            ;;
+    
+    esac
+done
