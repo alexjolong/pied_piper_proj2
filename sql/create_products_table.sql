d50fdcd8c5537fb9ff9193bb9b06023f900a6ca2
Sam Bloomquist <sambloomquist@Sams-MacBook-Pro.local>
2020-04-09 12:20:48 -0500

    cleaned up create products table script

--- sql/create_products_table.sql
+++ sql/create_products_table.sql
@@ -17,10 +17,9 @@ TBLPROPERTIES("skip.header.line.count"="1");
 
 -- create final products table, casting from float to decimal
 CREATE EXTERNAL TABLE IF NOT EXISTS ${var:dname}.Products
-AS SELECT ProductId, Name, cast(Price as DECIMAL(9, 2)) 
+AS SELECT ProductId, Name, cast(Price as DECIMAL(9, 2)) AS Price
 FROM pied_piper_sales_raw.Products1;
 
-ALTER TABLE pied_piper_sales_raw.Products CHANGE COLUMN _c2 Price DECIMAL(9,2);
 DROP TABLE pied_piper_sales_raw.Products1;
 
 -- reset metadata for table
