cbbf8f3e60e88a1e5480de964fe283ba03628f0f
ConmanEngineer <42391495+ConmanEngineer@users.noreply.github.com>
2020-04-10 23:49:43 -0500

    Update deploy-proj2.sh

--- bash/deploy-proj2.sh
+++ bash/deploy-proj2.sh
@@ -96,9 +96,9 @@ create_views() {
 
 create_partitions() {
     echo "Creating partition tables and views for Deliverable 3"
-    impala-shell -f "$sql_script_directory"/create_parition-product_sales_partition.sql
+    impala-shell -f "$sql_script_directory"/create_partition-product_sales_partition.sql
     impala-shell -f "$sql_script_directory"/create_view-customer_monthly_sales_2019_partitioned_view.sql
-    impala-shell -f "$sql_script_directory"/create_parition-product_region_sales_partition.sql
+    impala-shell -f "$sql_script_directory"/create_partition-product_region_sales_partition.sql
     echo "Done!"
 }
 
